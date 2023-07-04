447d69992faa3c66cde202beabc01c38fc6862c3
MariekeLouage <Marieke.Louage@UGent.be>
2023-07-04 16:51:44 +0200

    link together

--- vpr_batch.sh
+++ vpr_batch.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-for filename in $VSC_DATA/vpr/input/blif/*.blif; do
+for filepath in $VSC_DATA/vpr/input/blif/*.blif; do
 		echo $filename
         #qsub ./MyProgram.exe "$filename" "Logs/$(basename "$filename" .txt)_Log$i.txt"
+		
+		filename=$(basename -- "$filepath")
+		CIRCUIT="${filename%.*}"
+		
+		echo $CIRCUIT
+		qsub -F "$filepath $filename" vpr_job.pbs
 
 done
