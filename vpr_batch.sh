9e8ed29b135937e1c8ec3407115d3fc4112a03c2
vsc43465 vscuser <vsc43465@gligar07.gastly.os>
2023-07-06 20:25:06 +0200

    First succesful runs

--- vpr_batch.sh
+++ vpr_batch.sh
@@ -7,6 +7,6 @@ for filepath in $VSC_DATA/vpr/input/blif/*.blif; do
 		
 		echo $CIRCUIT
 		#-F "$filepath $filename"
-		qsub  vpr_job.pbs
-
+		#qsub -v filename=$CIRCUIT INPUT_FILE=$filepath vpr_job.pbs
+		qsub -v filename=$CIRCUIT,INPUT_FILE=$filepath vpr_job.pbs
 done
